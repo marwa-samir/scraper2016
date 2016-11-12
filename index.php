72f35331cf0f487551510448de77627b7797be05
marwa-samir <marwa.samir@mail.com>
2016-11-12 17:12:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ if (isset($accessToken)) {
   	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
 	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-	$loginUrl = $helper->getLoginUrl('http://mydemo.esy.es/php-graph-sdk/', $permissions);
+	//$loginUrl = $helper->getLoginUrl('http://mydemo.esy.es/php-graph-sdk/', $permissions);
+	$loginUrl = $helper->getLoginUrl('https://scraper2016.herokuapp.com/', $permissions);
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
